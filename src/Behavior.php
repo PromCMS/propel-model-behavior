7fef11bda10b0a48ac64a85a46ddbdc0edf810e2
Ondřej Langr <hi@ondrejlangr.cz>
2023-12-14 21:37:42 +0100

    update: Improves and filizes metadata.

--- src/Behavior.php
+++ src/Behavior.php
@@ -2,17 +2,18 @@
 
 namespace PromCMS\Core\Propel\Behaviors\PromModel;
 
+use Propel\Generator\Behavior\I18n\I18nBehavior;
 use Propel\Generator\Model\Column;
 use Propel\Generator\Model\Table;
 
 class Behavior extends \Propel\Generator\Model\Behavior
 {
   static string $PROM_ATTRIBUTE_NAMESPACE = 'prom';
-  static string $PROM_ADMIN_METADATA_ATTRIBUTE_NAMESPACE = "prom.adminmetadata";
-
   public function objectMethods()
   {
-    $script = $this->addGetPromMetadata();
+    $script = '';
+    $script .= $this->addGetPromMetadata();
+    $script .= $this->addColumnsMetadata();
     return $script;
   }
 
@@ -27,31 +28,80 @@ class Behavior extends \Propel\Generator\Model\Behavior
     $arr = $value;
   }
 
-  private function booleanToString(bool $value)
+  private function arrayToStringArray(array $input, $prefix = "[", $suffix = ']')
   {
-    return $value ? "true" : "false";
-  }
+    $output = $prefix;
 
-  private function getAdminMetadata(Column|Table $obj)
-  {
-    $attributes = $obj->getAttributes();
-    $attributeNamespace = static::$PROM_ADMIN_METADATA_ATTRIBUTE_NAMESPACE . '.';
-    $attributeNames = array_filter($attributes, fn($value) => str_starts_with($value, $attributeNamespace));
-    $result = [];
+    foreach ($input as $key => $value) {
+      $valueAsString = $value;
 
-    foreach ($attributeNames as $attributeName) {
-      $value = $obj->getAttribute($attributeName);
-
-      if ($value === 'false' || $value === 'true') {
-        $value = $value === 'true';
-      } else if (is_numeric($value)) {
-        $value = intval($value);
+      if (is_array($valueAsString)) {
+        $valueAsString = $this->arrayToStringArray($value);
+      } else {
+        $valueAsString = json_encode($valueAsString);
       }
 
-      $this->assignArrayByPath($result, str_replace($attributeNamespace, '', $attributeName), $value);
+      $output .= "'$key' => $valueAsString,";
+    }
+
+    $output .= $suffix;
+
+    return $output;
+  }
+
+  private function getPromMetadata(Column|Table $obj)
+  {
+    $attributes = $obj->getAttributes();
+    $attributeNamespace = static::$PROM_ATTRIBUTE_NAMESPACE . '.';
+
+    $attributeNames = array_filter($attributes, fn($key) => str_starts_with($key, $attributeNamespace), ARRAY_FILTER_USE_KEY);
+    $result = [];
+
+    foreach ($attributeNames as $attributeName => $attributeValue) {
+      if ($attributeValue === 'false' || $attributeValue === 'true') {
+        $attributeValue = $attributeValue === 'true';
+      } else if (is_numeric($attributeValue)) {
+        $attributeValue = intval($attributeValue);
+      }
+
+      $attributeName = str_replace('adminmetadata', 'adminMetadata', $attributeName);
+      $attributeName = str_replace('ignoreseeding', 'ignoreSeeding', $attributeName);
+
+      $this->assignArrayByPath($result, str_replace($attributeNamespace, '', $attributeName), $attributeValue);
     }
 
     return $result;
+  }
+
+  protected function addColumnsMetadata()
+  {
+    $table = $this->getTable();
+    $columns = $table->getColumns();
+    $localizedColumns = [];
+    $columnsAsMetadata = [];
+
+    /**
+     * @var I18nBehavior $localizedBehaviorInfo
+     */
+    if ($localizedBehaviorInfo = $table->getBehavior('i18n')) {
+      $localizedColumns = array_map(fn(Column $column) => $column->getName(), $localizedBehaviorInfo->getI18nColumns());
+    }
+
+    foreach ($columns as $column) {
+      $columnMetadata = $this->getPromMetadata($column);
+      $columnName = $column->getName();
+
+      $columnMetadata['required'] = $column->isNotNull();
+      $columnMetadata['unique'] = $column->isUnique();
+      $columnMetadata['translations'] = in_array($columnName, $localizedColumns);
+      $columnMetadata['autoIncrement'] = $column->isAutoIncrement();
+
+      $columnsAsMetadata[$columnName] = $columnMetadata;
+    }
+
+    return "
+private static \$promCmsColumnsMetadata = " . $this->arrayToStringArray($columnsAsMetadata) . ";
+";
   }
 
   protected function addGetPromMetadata()
@@ -59,37 +109,41 @@ class Behavior extends \Propel\Generator\Model\Behavior
     $table = $this->getTable();
     $tablePhpName = $table->getPhpName();
 
-    $hasSoftDelete = $this->booleanToString($table->hasBehavior('archivable'));
-    $hasTimestamps = $this->booleanToString($table->hasBehavior('timestampable'));
-    $hasOrdering = $this->booleanToString($table->hasBehavior('sortable'));
-    $isDraftable = $this->booleanToString(false);
+    $hasSoftDelete = json_encode($table->hasBehavior('archivable'));
+    $hasTimestamps = json_encode($table->hasBehavior('timestampable'));
+    $hasOrdering = json_encode($table->hasBehavior('sortable'));
+    $isDraftable = json_encode(false);
 
-    $ignoreSeeding = $table->getAttribute('prom.ignoreseeding', 'false');
     $isSharable = $table->getAttribute('prom.sharable', 'false');
     $isOwnable = $table->getAttribute('prom.ownable', 'false');
 
-    // $tableMetadata = $this->getAdminMetadata($table);
+    $tableMetadata = $this->getPromMetadata($table);
+    $icon = $tableMetadata['adminMetadata']['icon'];
 
     return "
+private static \$promCmsMetadata = [
+  " . $this->arrayToStringArray($tableMetadata, "", "") . "
+  /** @deprec */
+  'icon' => '$icon',
+  /** @deprec */
+  'admin' => " . $this->arrayToStringArray($tableMetadata['adminMetadata']) . ", 
+  'tableName' => ($tablePhpName::TABLE_MAP)::TABLE_NAME,
+  'hasTimestamps' => $hasTimestamps,
+  'hasSoftDelete' => $hasSoftDelete,
+  'columns' => static::\$promCmsColumnsMetadata,
+  'hasOrdering' => $hasOrdering,
+  'isDraftable' => $isDraftable,
+  'isSharable' => $isSharable,
+  'ownable' => $isOwnable,
+];
+
 /**
  * Gets table, and it's columns, metadata
  *
  */
-public static function getPromCMSMetadata()
+public static function getPromCmsMetadata()
 {
-  return [
-    'icon' => '', // This will be added later
-    'ignoreSeeding' => $ignoreSeeding,
-    'admin' => [], // This will be added later
-    'tableName' => ($tablePhpName::TABLE_MAP)::TABLE_NAME,
-    'hasTimestamps' => $hasTimestamps,
-    'hasSoftDelete' => $hasSoftDelete,
-    'columns' => [], // This will be added later
-    'hasOrdering' => $hasOrdering,
-    'isDraftable' => $isDraftable,
-    'isSharable' => $isSharable,
-    'ownable' => $isOwnable,
-  ];
+  return static::\$promCmsMetadata;
 }
 ";
   }
